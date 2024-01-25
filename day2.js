d3b1eec6f4737d916cfbfde77ecee7a206098d90
abdalla hassan <abdalla472002@gmail.com>
2024-01-25 03:13:44 +0200

    day2m

--- day2.js
+++ day2.js
@@ -11,3 +11,11 @@ var F = 40;
 var C;
 C= (F-32) * 5/9;
 console.log(C);
+// if statement //
+var namme="ahmed";
+var id= 10;
+var age=25;
+
+ if( namme == "ahmed" && id==10 && age >=20 ){
+    console.log("allowed");
+ }
