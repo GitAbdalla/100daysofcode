de1625f7f8eeab464108387715615bb150f09efb
abdalla hassan <abdalla472002@gmail.com>
2024-02-03 22:04:37 +0200

    day8

--- day8/day8.js
+++ day8/day8.js
@@ -1,1 +1,10 @@
-/* Common JS */
+/* -modules-  [Common JS] 
+we can make our module like ourmath
+or built in modules
+or third-party modules 
+*/
+
+let math = require('./ourmath');
+
+console.log(math.fact(8));
+console.log(math.circleArea(10));
