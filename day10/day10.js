8215df87e671e8bfcb08974be4874acdd1ce394b
abdalla hassan <abdalla472002@gmail.com>
2024-02-05 23:49:32 +0200

    day10

---
+++ day10/day10.js
@@ -0,0 +1,27 @@
+/* – Numbers – Number object */
+
+console.log(Number( true)); 
+console.log(Number(false));
+console.log(Number(undefined));
+console.log(Number(null));
+console.log(Number("10"));
+console.log(Number("    10"));
+console.log(Number("    10    "));
+console.log(Number("  10   1 "));
+console.log("-----------------")
+console.log(Number.NaN == Number.NaN);
+console.log(Number.NaN === Number.NaN);
+console.log("-----------------")
+console.log(Number.isNaN(undefined));
+console.log(Number.isNaN(null));
+console.log(Number.isNaN("15"));
+
+console.log("-----------------")
+
+console.log( isNaN(undefined));
+console.log( isNaN(null));
+console.log( isNaN("15")); 
+console.log("-----------------")
+console.log(Number.isFinite("0"));
+
+console.log(isFinite("0"));
