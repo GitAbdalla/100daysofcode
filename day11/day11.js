0ea1d45370fce77df7e6c2108d124b9c2e378558
abdalla hassan <abdalla472002@gmail.com>
2024-02-06 23:43:04 +0200

    day11

--- day11/day11.js
+++ day11/day11.js
@@ -4,3 +4,18 @@ console.log(Math.abs(-10));
 console.log(Math.abs(+10));
 
 console.log(Math.cbrt(27));
+
+console.log(Math.ceil(123.98));
+
+console.log(Math.floor(123.98));
+
+console.log(Math.log(100));
+console.log(Math.random());
+
+console.log(Math.sqrt(16));
+
+console.log(Math.round(12.6));
+
+
+
+
