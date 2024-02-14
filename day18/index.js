f42176b2bbb590abea911645649dd0fe6ba8c39c
abdalla hassan <abdalla472002@gmail.com>
2024-02-14 23:14:48 +0200

    package

---
+++ day18/index.js
@@ -0,0 +1,9 @@
+// i published my first package to npm and i installed it
+
+const ourMath = require('ourmath');
+
+let average =ourMath.avg(1,2,3,4,5,6,7,8);
+console.log(average);
+
+let inter_set=ourMath.intersection([1,2,4],[2,3,4]);
+console.log(inter_set);
