7daed8af19ecfa7405cfdccadfff69bd33d5080c
abdalla hassan <abdalla472002@gmail.com>
2024-02-25 22:27:55 +0200

    Binary search

---
+++ day25/day25.js
@@ -0,0 +1,21 @@
+// implementation of binary search
+function bainary_search(list, item) {
+  let low = 0;
+  let high = list.length - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + high / 2);
+    let guess = list[mid];
+    if (guess === item) {
+      return mid;
+    }
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+}
+let my_list = [1, 3, 5, 7, 9];
+console.log(bainary_search(my_list, 3));
+console.log(bainary_search(my_list, -5));
