4ff8d114073da8cda7af3f42e976adbcf76cd696
abdalla hassan <abdalla472002@gmail.com>
2024-03-02 22:06:56 +0200

    in series nested callback

--- day31/index.js
+++ day31/index.js
@@ -1,1 +1,16 @@
 // – Asynchronous – Flow Control Patterns .. 
+
+
+// in series neested callbacks
+let fs = require('fs');
+fs.readFile('./01.txt', { encoding: 'utf-8' }, function (err, data) {
+    let data_01 = "--" + data + "--";
+    fs.writeFile('./new_01.txt', data_01, function (err) {
+        fs.readFile('./02.txt', { encoding: 'utf-8' }, function (err, data) {
+            let data_02 = "--" + data + "--";
+            fs.writeFile('.//new_02.txt', data_02, function (err) {
+                console.log('new_02.txt created.');
+            })
+        })
+    })
+})
