4ede55a5cc1bab3803a2bf2567bb508f5ba34ebb
abdalla hassan <abdalla472002@gmail.com>
2024-03-04 22:41:59 +0200

    Async & Sync

--- day33/node.js
+++ day33/node.js
@@ -11,4 +11,20 @@ const first = () => {
     console.log("The end")
 }
 
+
 first()
+
+console.log("-------------")
+// Async
+const third = () => {
+    setTimeout(() =>{
+        console.log("Async hey there")
+    },2000)
+}
+const fourth = () => {
+
+    console.log("Hey there")
+    third()
+    console.log("The end")
+}
+fourth()
