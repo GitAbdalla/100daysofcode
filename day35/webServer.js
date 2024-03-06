d63f44abc649698f02990ab947e39d69cd1e372f
abdalla hassan <abdalla472002@gmail.com>
2024-03-06 02:02:47 +0200

    httpServer

--- day35/webServer.js
+++ day35/webServer.js
@@ -1,12 +1,32 @@
 const HTTPModule = require("http")
 const port = 3333
 let HTTPServer = HTTPModule.createServer()
-HTTPServer.on('request',(request,response) => {
+HTTPServer.on('request', (request, response) => {
     console.log("request recieved")
-    response.write("hello client")
+    console.log(request.method)
+    let url = request.url
+    switch (url) {
+        case '':
+            response.write("<h1>Hello,this is home page</h1>")
+            break;
+
+        case '/home':
+            response.write("<h1>Hello,this is home page</h1>")
+            break;
+        case '/about':
+            response.write("<h1>Hello,this is about page</h1>")
+            break;
+        case '/contact':
+            response.write("<h1>Hello,this is contact page</h1>")
+            break;
+
+        default:
+            response.write("<h1>404</h1>")
+
+    }
     response.end()
 
 })
-HTTPServer.listen(port,()=>{
+HTTPServer.listen(port, () => {
     console.log(`server started on port: ${port}`)
 })
