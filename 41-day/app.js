ef3d8e8b285807b89b62bd030d3f5d4ee3fa50a3
abdalla hassan <abdalla472002@gmail.com>
2024-03-12 17:32:31 +0200

    Express.js Register & Login Backend

--- 41-day/app.js
+++ 41-day/app.js
@@ -8,7 +8,7 @@ const port =3002
  app.use(express.json())
 
 
- app.post('/register' , (req,res) => {
+ app.post('/register' , async (req,res) => {
     try {
         const { email,password } = req.body
         //find user
@@ -17,10 +17,32 @@ const port =3002
             res.status(400).send("wrong email or password !")
         }
         // hash password
-        
+        const hashPssword = await bcrypt.hash(password,10)
+        users.push({email,password: hashPssword})
+        console.log(users)
+        res.status(201).send("registered successfully")
         
     } catch (error) {
-        res.status(500).send((message: error.message ))
+        res.status(500).send({message: error.message })
+    }
+ })
+
+ app.post("/login", async (req ,res) => {
+    try {
+        const { email,password } = req.body
+        //find user
+        const findUser = users.find((data) => email == data.email)
+        if (!findUser){
+            res.status(400).send("wrong email or password !")
+        }
+        const passwordMatch = await bcrypt.compare(password , findUser.password)
+        if (passwordMatch){
+            res.status(200).send("Logged in successfully")
+        }else{
+            res.status(400).send("wrong email or password !")
+        }
+    } catch (error) {
+        res.status(500).send({message: error.message })
     }
  })
 
