a0670a17f613b1f45b790612372afec7e79c0ac3
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2024-03-25 10:54:13 +0200

    to do

--- 42-day/index.js
+++ 42-day/index.js
@@ -1,28 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const tasksRoutes = require('./routes/tasks')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import the CORS middleware
 
-const app = express()
-const port = 3001
+const tasksRoutes = require('./routes/tasks');
+
+const app = express();
+const port = 3001;
+
+// Enable CORS middleware
+app.use(cors());
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // DB Connection
-mongoose.connect('mongodb://localhost:27017/task/todo')
+mongoose.connect('mongodb://localhost:27017/task');
 
 const db = mongoose.connection;
 
 db.on('error', () => {
-    console.log("Connection Error!")
-})
+    console.log("Connection Error!");
+});
 
 db.once('open', () => {
-    console.log('Connected to DB!')
-})
+    console.log('Connected to DB!');
+});
 
-app.use(tasksRoutes)
+app.use(tasksRoutes);
 
 app.listen(port, () => {
-    console.log("Server started on port 3000")
-})
+    console.log("Server started on port", port);
+});
--- 42-day/models/schema.js
+++ 42-day/models/schema.js
@@ -1,9 +1,9 @@
-const mongoose = require('momgoose')
+const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    title:{type:String,required:true},
-    description:{type:String,required:false},    
-    completed:{type:Boolean,default:false}    
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    completed: { type: Boolean, default: false }
+});
 
-})
-module.exports = mongoose.model('Task',taskSchema)
+module.exports = mongoose.model('Task', taskSchema);
